1c70e25c0b3745d658ba150c1794f2f7311019cc
elib00 <joshuanapinas19@gmail.com>
2024-06-19 16:11:54 +0800

    [Joshua] Added api route

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -4,8 +4,9 @@ export const GET = async () => {
     return NextResponse.json({success: true})
 };
 
-export const POST = async (req: NextRequest) => {
-    try{
-        const body = await req.json();
-    }
-}
+// export const POST = async (req: NextRequest) => {
+//     try{
+//         const body = await req.json();
+//         const 
+//     }
+// }
