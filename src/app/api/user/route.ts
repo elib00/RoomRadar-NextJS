c8f7155651874ee032b00205fd15861e69a52721
elib00 <joshuanapinas19@gmail.com>
2024-06-19 16:11:14 +0800

    [Joshua] Added new models

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async () => {
+    return NextResponse.json({success: true})
+};
+
+export const POST = async (req: NextRequest) => {
+    try{
+        const body = await req.json();
+    }
+}
